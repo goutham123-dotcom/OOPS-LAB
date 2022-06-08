3a0dca9e7a421d11cf3a32e304be750121c780de
goutham123-dotcom <55642670+goutham123-dotcom@users.noreply.github.com>
2022-06-08 15:38:34 +0530

    Add files via upload

---
+++ File1/Main.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+ import java.io.FileReader;
+ import java.io.FileWriter;
+ import java.io.IOException;
+public class Main {
+ public static void main(String[] args) {
+ try {
+ FileWriter writer = new FileWriter("file1.txt",true);
+ writer.write("new file is created");
+ writer.close();
+ FileReader reader = new FileReader("java_write.txt");
+ BufferedReader br= new BufferedReader(reader);
+ String line;
+ System.out.println("Data read from the file");
+
+ while ((line = br.readLine()) != null) {
+ System.out.println(line);
+ }
+ reader.close();
+ } catch (IOException e) {
+ System.out.println("-----Error-----");
+ }
+ }
+}
